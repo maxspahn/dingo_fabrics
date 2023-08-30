4b3c7935df7edf81c9d66a107988f944a8450ae9
Max Spahn <m.spahn@tudelft.nl>
2023-08-30 22:02:52 +0200

    Adds first running urdf file of dingo.

--- examples/run_simple_dingo_environment.py
+++ examples/run_simple_dingo_environment.py
@@ -1,6 +1,8 @@
+import numpy as np
 from dingo_fabrics.create_environment import init_env
 
 if __name__ == "__main__":
-    env = init_env()
-    for _ in range(100):
-        env.step()
+    env = init_env(render=True)
+    action = np.array([0.3, 0.4, 1.0])
+    for _ in range(1000):
+        env.step(action)
